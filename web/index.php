e5dcce8131476991f2fa73c5114d6d985969b83f
renato <renato@renato-PC>
2015-12-20 20:20:41 +0000

    Added sound to Bird this fixes #13

--- web/index.php
+++ web/index.php
@@ -2,27 +2,37 @@
 
 require 'Slim/Slim.php';
 
-function loadClass($name) {
-    $classes = array(
-        'Config' => '../config/Config.php',
-        'Error' => '../validation/Error.php',
-        'Flash' => '../flash/Flash.php',
-        'NotFoundException' => '../exception/NotFoundException.php',
-        'TodoDao' => '../dao/TodoDao.php',
-        'TodoMapper' => '../mapping/TodoMapper.php',
-        'Todo' => '../model/Todo.php',
-        'TodoSearchCriteria' => '../dao/TodoSearchCriteria.php',
-        'TodoValidator' => '../validation/TodoValidator.php',
-        'Utils' => '../util/Utils.php',
-    );
-    if (!array_key_exists($name, $classes)) {
-        die('Class "' . $name . '" not found.');
-    }
-    require_once $classes[$name];
-}
+require_once('../config_1/Config.php');
+require_once('../validation/Error.php');
+require_once('../flash/Flash.php');
+require_once('../exception/NotFoundException.php');
+require_once('../dao/DAO.php');
+require_once('../dao/AutorizacaoDao.php');
+require_once('../dao/UnidadesDao.php');
+require_once('../dao/EncOleosDao.php');
+require_once('../dao/firmaDao.php');
+require_once('../dao/UtilizadorDao.php');
+require_once('../dao/EquipamentoDao.php');
+require_once('../dao/RelatorioDao.php');
+require_once('../dao/PedidosTrabalhoDao.php');
+require_once('../dao/ProcessoDao.php');
+require_once('../mapping/EncOleosMapper.php');
+require_once('../mapping/AutorizacaoMapper.php');
+require_once('../mapping/PedidosTrabalhoMapper.php');
+require_once('../mapping/UtilizadorMapper.php');
+require_once('../model/Autorizacao.php');
+require_once('../model/Utilizador.php');
+require_once('../model/PedidosTrabalho.php');
+require_once('../model/EncOleos.php');
+require_once('../validation/AutorizacaoValidator.php');
+require_once('../validation/UtilizadorValidator.php');
+require_once('../validation/PedidosTrabalhoValidator.php');
+require_once('../util/Utils.php');
+require_once('../pdf/fpdf.php');
+require_once('../php-ews/sendmail.php');
 
 \Slim\Slim::registerAutoloader();
-spl_autoload_register('loadClass');
+//spl_autoload_register('loadClass');
 session_start();
 
 $app = new \Slim\Slim(array(
@@ -37,7 +47,9 @@ $app->get('/', function() use ($app) {
         });
 
 
-
+$app->post('/login',function() use ($app){
+            require '../page/home.html';
+});
 
 $app->run();
 ?>
