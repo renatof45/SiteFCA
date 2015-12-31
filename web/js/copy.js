0a652b8db8ef096a53c6ffb3b72ff0902d943541
renato <renato@renato-PC>
2015-12-31 16:24:48 +0000

    Added sound to Bird this fixes #13

---
+++ web/js/copy.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+var cur = "&copy; ";
+var year = new Date();
+if(cur == year.getFullYear()) year = year.getFullYear(); else year = cur +  year.getFullYear(); document.write(year);  
+
+
